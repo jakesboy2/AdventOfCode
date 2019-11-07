8e0cb1278da7244b04aa8c08011f86416cda6e0a
Jacob Waldrip <jacob.waldrip@egov.com>
2019-11-06 18:33:29 -0600

    prepared day 3 empty files

---
+++ 2018/day3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    ifstream inputFile;
+    inputFile.open("./input.txt");
+    string line;
+    
+    while(inputFile >> line) {
+
+    }
+
+    return 0;    
+}
